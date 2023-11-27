9e24e66a3326060d604cb91ff955562d03126289
D FERGUSSON <J233195@tafe.wa.edu.au>
2023-11-27 20:48:55 +0800

    feat(home): reposition header items below fold

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,9 @@
 const initApp = () => {
     const hamburgerButton = document.getElementById('hamburger-button')
     const mobileMenu = document.getElementById('mobile-menu')
+    const desktopMenu = document.getElementById('desktop-menu')
+    const headerName = document.getElementById('header-name')
+    const splashName = document.getElementById('splash-name')
 
     const toggleMenu = () => {
         mobileMenu.classList.toggle('hidden')
@@ -9,6 +12,24 @@ const initApp = () => {
 
     hamburgerButton.addEventListener('click', toggleMenu)
     mobileMenu.addEventListener('click', toggleMenu)
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) { // Above the fold
+                headerName.classList.add('invisible');
+                desktopMenu.classList.add('left-1/2', 'transform', '-translate-x-1/2');
+                desktopMenu.classList.remove('right-4');
+            } else { // Below the fold
+                headerName.classList.remove('invisible');
+                desktopMenu.classList.remove('left-1/2', 'transform', '-translate-x-1/2');
+                desktopMenu.classList.add('right-4');
+            }
+        })
+    }, {threshold: 0.9})
+
+    if (splashName) {
+        observer.observe(splashName)
+    }
 }
 
 document.addEventListener('DOMContentLoaded', initApp)
